5ba85ca80da679fcbc270cafa5c3de3a7d74ee0b
Sanghyeon Sim <5338095@gmail.com>
2023-08-22 10:37:26 +0900

    feat : init device cache

--- lib/src/features/common/splash/viewmodel/splash_view_model.dart
+++ lib/src/features/common/splash/viewmodel/splash_view_model.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:seenear/core/base/model/base_view_model.dart';
+import 'package:seenear/core/init/db/local_manager.dart';
 
 class SplashViewModel extends BaseViewModel with ChangeNotifier {
   @override
-  void init() {}
+  void init() {
+    controlAppState();
+  }
+
+  Future<void> controlAppState() async {
+    await deviceAndCacheInit();
+  }
+
+  Future<void> deviceAndCacheInit() async {
+    await Future.wait([LocaleManager.prefrencesInit()]);
+  }
 
   @override
   void setContext(BuildContext context) {

7be55857c7c62e2b6a9b968a99ff0af6bb0557bd
Sanghyeon Sim <5338095@gmail.com>
2023-08-29 21:58:31 +0900

    feat : card screen

---
+++ lib/src/features/junior/cards/card_screen_view_model.dart
@@ -0,0 +1,11 @@
+import 'package:seenear/core/base/model/base_view_model.dart';
+
+class CardScreenViewModel extends BaseViewModel {
+  @override
+  void init() {}
+
+  @override
+  void setContext(context) {
+    viewModelContext = context;
+  }
+}

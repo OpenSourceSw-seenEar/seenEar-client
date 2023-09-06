2104638b3cb6f18a2222f003cbfe056c788c4605
Sanghyeon Sim <5338095@gmail.com>
2023-09-06 23:08:01 +0900

    feat : 노인 / 조언 종류 선택 view model

---
+++ lib/src/features/senior/advice/select_advice_type/select_advice_type_view_model.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:seenear/core/base/model/base_view_model.dart';
+
+class SelectAdviceTypeViewModel extends BaseViewModel {
+  @override
+  void init() {}
+
+  @override
+  void setContext(BuildContext context) {}
+}

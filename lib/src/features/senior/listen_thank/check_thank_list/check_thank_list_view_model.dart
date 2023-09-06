d4493332640a40aeafd7c6194f3aaab81563b24b
Sanghyeon Sim <5338095@gmail.com>
2023-09-06 23:02:03 +0900

    feat: 노인 / 감사 편지함 view model

---
+++ lib/src/features/senior/listen_thank/check_thank_list/check_thank_list_view_model.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'package:seenear/core/base/model/base_view_model.dart';
+
+class CheckThankListViewModel extends BaseViewModel {
+  @override
+  void init() {}
+
+  @override
+  void setContext(BuildContext context) {}
+}

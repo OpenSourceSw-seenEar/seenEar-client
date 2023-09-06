43bef8ae3ebeb7ba9d58b1a6d964a7599f23d40d
Sanghyeon Sim <96258104+halfmoon-mind@users.noreply.github.com>
2023-09-06 19:45:24 +0900

    [#31] enum 및 map 추가 (#32)
    * feat: 채팅 유저 구분 enum case 추가
    
    * feat: 성별 선택 enum 추가
    
    * feat: 걱정 타입 map 추가

---
+++ lib/src/constants/enums/chat_person_enum.dart
@@ -0,0 +1,1 @@
+enum ChatPersonType { senior, junior }
---
+++ lib/src/constants/map/worry_map.dart
@@ -0,0 +1,23 @@
+const worryTypeMap = {
+  "인간관계": "worry_relation",
+  "가족": "worry_family",
+  "건강": "worry_health",
+  "결혼": "worry_marriage",
+  "꿈/공부": "worry_dream",
+  "돈": "worry_money",
+  "진로": "worry_career",
+  "회사": "worry_company",
+  "기타": "worry_etc"
+};
+
+const worryTypeMapReverse = {
+  "PERSON": "인간관계",
+  "FAMILY": "가족",
+  "worry_health": "건강",
+  "worry_marriage": "결혼",
+  "worry_dream": "꿈/공부",
+  "worry_money": "돈",
+  "worry_career": "진로",
+  "worry_company": "회사",
+  "worry_etc": "기타"
+};
